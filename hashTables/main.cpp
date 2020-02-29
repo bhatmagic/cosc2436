fe0b699c01c2c4947ef2da830caccf9ba88ed1cf
dboyd42 <bhatmagician@gmail.com>
2020-02-29 00:05:04 -0600

    updated output and comments for readability

--- hashTables/main.cpp
+++ hashTables/main.cpp
@@ -13,73 +13,131 @@ Completed: 2018.11.27
 
 int main(int argc, char **argv) {
 
-    // Check argument count
+    // ===
+    // === Validate arguments
+    // ===
     if (argc != 2) {
         std::cout << "[*] Hash Table" << std::endl;
         std::cout << "[*] Usage:    : argv[1] <data.csv>" << std::endl;
         return 0;
     }
-
-    // initialize dataFile to filename
+    // Initialize dataFile from argv[1]
     std::string dataFile = argv[1];
-
     // Check if file exists
     if (!test(dataFile)) { return 0; }
 
-    // create hash object & table
-    std::cout<<"\nCreating Object\n";
+    // ===
+    // === Create hash object & table
+    // ===
+    std::cout << "Creating Object and Table" << std::endl;
+    std::cout << "=========================" << std::endl;
     HashTable hashObj;
-    std::cout<<"createHashTable\n";
+    std::cout << "Created hash object" << std::endl;
     createHashTable(&hashObj, dataFile);
-    std::cout<<"HashTable created\n";
+    std::cout << "Created hash table with " << dataFile << std::endl;
 
-    std::cout<<"traverse():\n";
+    // ===
+    // === [TEST] Traversing Hash Table
+    // ===
+    std::cout << std::endl;
+    std::cout << "[TEST] Traverse Method" << std::endl;
+    std::cout << "======================" << std::endl;
+    // Traverse table
+    std::cout << "obj.traverse()" << std::endl;
+    std::cout << "----------------------" << std::endl;
     hashObj.traverse();
-    std::cout << "getNumberOfEntries:"
-              << SPACE << hashObj.getNumberOfEntries() << std::endl;
-    std::cout<<"isEmpty():"<<SPACE;
-    hashObj.isEmpty() ? std::cout<<"Yes\n" : std::cout<<"No\n";
-
-    // display array, contains
-    int arr[] = {100, 108, 117, 129, 157, 205, 155, 131, 156,
-                 195, 115, 208, 132, 1, 2, 3, 4, 5, 6};
-    std::cout<<"\ncontains(arr[]):\n----------------------------------\n";
-    for (int i = 0; i < TABLE_SIZE; ++i) {
-        std::cout<<std::setw(3)<<arr[i]<<SPACE;};
-    std::cout<<std::endl;
-    for (int i = 0; i < TABLE_SIZE; ++i) {
-        hashObj.contains(arr[i]) ? std::cout<<std::setw(3)<<"Yes"<<SPACE
-                                 : std::cout<<std::setw(3)<<"No"<<SPACE;}
-    std::cout<<std::endl;
-
-    // Remove nodes
-    std::cout<<"\nremove(117):"<<SPACE;
-    hashObj.remove(117) ? std::cout<<"Yes\n" : std::cout<<"Nope\n";
-    std::cout<<"remove(54):"<<SPACE;
-    hashObj.remove(54) ? std::cout<<"Yes\n" : std::cout<<"Nope\n";
-    std::cout<<"remove(195):"<<SPACE;
-    hashObj.remove(195) ? std::cout<<"Yes\n" : std::cout<<"Nope\n";
-    std::cout<<"remove(6):"<<SPACE;
-    hashObj.remove(6) ? std::cout<<"Yes\n" : std::cout<<"Nope\n";
-    hashObj.traverse();
-    std::cout << "getNumberOfEntries:"
-              << SPACE << hashObj.getNumberOfEntries() << std::endl;
-
-
-    std::cout<<"getData(131):" << SPACE << hashObj.getData(131);
-
-    std::cout<<std::endl;
-    std::cout<<"clear():"<<SPACE;
-    hashObj.clear() ? std::cout<<"Yes.\n" : std::cout<<"NOT cleared.\n";
-
-    std::cout << "getNumberOfEntries: "
+    // Print accessors
+    std::cout << "----------------------" << std::endl;
+    std::cout << "obj.getNumberOfEntries(): "
               << hashObj.getNumberOfEntries() << std::endl;
+    std::cout << "obj.isEmpty()   : ";
+    hashObj.isEmpty() ? std::cout << "Yes\n" : std::cout << "No\n";
+    std::cout << "obj.getData(131): " << hashObj.getData(131) << std::endl;
+    std::cout << "obj.getData(117): " << hashObj.getData(117) << std::endl;
 
-    std::cout<<"traverse(): <post-clear>"<<SPACE;
-    hashObj.traverse() ? std::cout<<"Fu duq?\n" : std::cout<<"Nope\n";
+    // ===
+    // === [TEST] Contains method with a set of keys
+    // ===
+    int testKeysArr[] = {100, 108, 117, 129, 157, 205, 155, 131, 156,
+                         195, 115, 208, 132, 1, 2, 3, 4, 5, 6};
+    std::cout << std::endl;
+    std::cout << "[TEST] Contains Method" << std::endl;
+    std::cout << "======================" << std::endl;
+    // Print keys in testKeysArr
+    std::cout << "obj.contains(KEY) : ";
+    for (int i = 0; i < TABLE_SIZE; ++i)
+    {
+        std::cout << std::setw(3) << testKeysArr[i] << SPACE;
+    };
+    std::cout << std::endl;
+    // Print yes/no based off return from contains method
+    std::cout << "Boolean Return    : ";
+    for (int i = 0; i < TABLE_SIZE; ++i)
+    {
+        hashObj.contains(testKeysArr[i]) ?
+            std::cout << std::setw(3) << "Yes" << SPACE
+            : std::cout << std::setw(3) << "No" << SPACE;
+    };
+    std::cout<<std::endl;
 
+    // ===
+    // === [TEST] Removing nodes
+    // ===
+    std::cout << std::endl;
+    std::cout << "[TEST] Remove Method" << std::endl;
+    std::cout << "====================" << std::endl;
+    std::cout << "obj.remove(117): ";
+    // Remove random nodes
+    hashObj.remove(117) ? std::cout << "Removed\n"
+                        : std::cout << "No existing key\n";
+    std::cout << "obj.remove(195): ";
+    hashObj.remove(195) ? std::cout << "Removed\n"
+                        : std::cout << "No existing key\n";
+    std::cout<<"obj.remove(54) : ";
+    hashObj.remove(54) ? std::cout << "Removed\n"
+                       : std::cout << "No existing key\n";
+    std::cout << "obj.remove(6)  : ";
+    hashObj.remove(6) ? std::cout << "Removed\n"
+                      : std::cout << "No existing key\n";
+    // Traverse table
+    std::cout << "----------------------" << std::endl;
+    std::cout << "obj.traverse()" << std::endl;
+    hashObj.traverse();
+    // Print accessors
+    std::cout << "----------------------" << std::endl;
+    std::cout << "obj.getNumberOfEntries(): "
+              << hashObj.getNumberOfEntries() << std::endl;
+    std::cout << "obj.isEmpty()   : ";
+    hashObj.isEmpty() ? std::cout << "Yes\n" : std::cout << "No\n";
+    std::cout << "obj.getData(131): " << hashObj.getData(131) << std::endl;
+    std::cout << "obj.getData(117): " << hashObj.getData(117) << std::endl;
 
+    // ===
+    // === [TEST] Clear hash object
+    // ===
+    std::cout << std::endl;
+    std::cout << "[TEST] Clear Method" << std::endl;
+    std::cout << "===================" << std::endl;
+    std::cout << "obj.clear(): ";
+    hashObj.clear() ? std::cout << "cleared\n" : std::cout << "NOT cleared.\n";
+    std::cout << "-------------------" << std::endl;
+    // Print accessors
+    std::cout << "----------------------" << std::endl;
+    std::cout << "obj.getNumberOfEntries(): "
+              << hashObj.getNumberOfEntries() << std::endl;
+    std::cout << "obj.isEmpty()   : ";
+    hashObj.isEmpty() ? std::cout << "Yes\n" : std::cout << "No\n";
+    std::cout << "obj.getData(131): " << hashObj.getData(131) << std::endl;
+    std::cout << "obj.getData(117): " << hashObj.getData(117) << std::endl;
+    std::cout << "obj.traverse()  : ";
+    hashObj.traverse() ? std::cout << "Shouldn't Be Here\n"
+                         : std::cout << "No existing table\n";
 
+    // ===
+    // === End Program
+    // ===
+    std::cout << std::endl;
     std::cout<<"This is the end of the program." << std::endl;
     return 0;
 }
+
